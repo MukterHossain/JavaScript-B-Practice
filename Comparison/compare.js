c71488d87397e0a260ec920f2802fa8be227c185
MukterHossain <mukter3075@gmail.com>
2025-04-03 21:53:05 +0600

    first commit

---
+++ Comparison/compare.js
@@ -0,0 +1,111 @@
+// JavaScript book page(68)
+
+// 1
+const brother1 = 7
+const brother2 = 5
+if(brother1 > brother2){
+    // console.log('Elder brother has eaten more sendwich')
+}else{
+    // console.log('Younger brother has eaten more sendwich')
+}
+// console.log(typeof brother1, typeof brother2)
+
+// 2
+const myClass = 45
+const sideClass = 50
+if(myClass > sideClass){
+    // console.log('Our Class is bigger')
+}else{
+    // console.log('Side Class is bigger')
+}
+
+// 3
+const myNumber = 75
+const friendNumber = 75
+if(myNumber == friendNumber){
+    // console.log('two numbers are equal')  
+ }else{
+    // console.log('two numbers are not equal')
+}
+
+// 4
+const tagetMoney = 100
+const savedMoney = 90
+if(tagetMoney <= savedMoney){
+    // console.log('You have to save more money')
+}else{
+    // console.log('You save less money')
+}
+// console.log(tagetMoney <= savedMoney)
+
+// 5
+// console.log(10 != 7)
+
+// 6
+// console.log(20 <= 15)
+
+// 7
+// console.log(45 > 97)
+
+
+// 8
+// console.log(50 >= 50)
+
+
+const peyaraPrice = 40 
+const applePrice = 250
+// console.log(peyaraPrice > applePrice)
+
+const myName= ('ToHiN').toLowerCase()
+const newName = ('tOhIn').toLowerCase()
+// console.log(myName == newName)
+
+const myPromise = true
+const friendPromise = false
+// console.log(myPromise != friendPromise)
+// console.log(myPromise == friendPromise)
+
+
+
+// Page (70)
+// 1
+const myAge = 15
+const elderBrotherAge =20
+// if(elderBrotherAge <= myAge){
+//     console.log('I am elder than my brother')
+// }else{
+//     console.log('I am younger than my brother')
+// }
+
+// 2
+const passMark = 33
+const myMark = 45
+// if(myMark >= passMark){
+//     console.log('I am pass')
+// }else{
+//     console.log('I am fail')
+// }
+
+// 3
+const jamburaPrice = 50
+const kotbelPrice = 30
+// console.log(jamburaPrice < kotbelPrice)
+
+
+// 4
+const IResult = 10
+const friendRead = 10
+    // console.log(IResult == friendRead)
+
+
+// 5
+const myBuiscut = 15
+const friendBuiscut = 20
+// console.log(myBuiscut < friendBuiscut)
+
+
+// 6
+const realmadred = 2
+const barcelona = 3
+// console.log(realmadred < barcelona)
+
---
+++ Data-Type/ParseNaN.js
@@ -0,0 +1,81 @@
+
+
+const myTaka = 2.3252325
+const newTaka = parseInt(myTaka)
+const yourTaka = parseFloat(myTaka)
+const TakaFixed = (myTaka.toFixed(2))
+const TakaFP = parseFloat(myTaka.toFixed(2))
+// console.log(myTaka, newTaka, yourTaka, TakaFixed, TakaFP)
+
+const fatherAge = '50'
+const sonAge = '20'
+const sumAge = (parseInt(fatherAge) + parseInt(sonAge))
+// console.log(sumAge)
+// const nowDate = '15'
+// const nowMonth = '05'
+// const nowYear = '2025'
+// const nowTime = nowDate + '/' + nowMonth + '/' + nowYear
+// console.log(nowTime)
+
+const nowDate = parseInt('15')
+const nowMonth = parseInt('05')
+const nowYear = parseInt('2025')
+const nowTime = nowDate + '/' + nowMonth + '/' + nowYear
+// console.log(nowTime)
+
+
+const inNew = parseInt('x30')
+// console.log(inNew)
+const price = parseInt('5price5')
+const age = parseInt('10year')
+const ageNew = parseInt("year15")
+// console.log(price , age, ageNew)
+const textNum = parseInt('transparant')
+const NumText = parseInt('tweenty')
+// console.log(textNum, NumText)
+
+
+const number = '12325'
+const newNumber = 12525
+// console.log(number , newNumber)
+
+
+
+// 1 
+
+const math = '20'
+// console.log(parseInt(math) + 10)
+
+// 2
+const numbers = '3.14159'
+// console.log(parseInt(number))
+
+//3
+const dataName = 'premikBoy'
+// console.log(parseInt(dataName))
+
+//4
+// console.log((parseInt('3.456')+ parseInt('2.789')))
+// console.log((parseInt('3.456')+ parseInt('2.789')).toFixed(2))
+// console.log(parseInt(parseInt('3.456')+ parseInt('2.789')).toFixed(2))
+// console.log((parseFloat('3.456')+ parseFloat('2.789')))
+// console.log((parseFloat('3.456')+ parseFloat('2.789')).toFixed(2))
+// console.log(parseInt(parseFloat('3.456')+ parseFloat('2.789')).toFixed(2))
+
+
+// 5 
+const num1= '56.78'
+const num2 = '12.34'
+const sum = parseInt(num1) + parseInt(num2)
+// console.log(sum)
+
+// 6 
+const num3 = '10.5678'
+const num4 = '5.4321'
+const total = parseFloat(num3) + parseFloat(num4)
+// console.log(total, total.toFixed(1))
+
+// 7
+const num5 = '42.45689754'
+console.log(parseFloat(num5).toFixed(3))
+console.log(parseFloat(num5))
