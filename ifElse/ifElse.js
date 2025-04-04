de402ab6cb45bd2bfffd1c7a17a6a5d36d925cea
MukterHossain <mukter3075@gmail.com>
2025-04-04 23:06:52 +0600

    added IfElse

---
+++ ifElse/ifElse.js
@@ -0,0 +1,17 @@
+
+const x = 100
+const y = 100
+
+if(x > y){
+    console.log('x is greater than y')
+}
+
+else if(x < y){
+    console.log('Your is equal to jlsjdj')
+}
+else if(x == y && x > y){
+    console.log('x22 is less than 2')
+}
+else{
+    console.log('y222111 is greater than 2')
+}
