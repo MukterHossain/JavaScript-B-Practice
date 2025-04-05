d5719fd0280020860f0b1acef2db81f17436258f
MukterHossain <mukter3075@gmail.com>
2025-04-05 23:12:03 +0600

    added some code in ifelse

--- ifElse/ifElse.js
+++ ifElse/ifElse.js
@@ -92,7 +92,136 @@ if(salary > 50000){
 
 const isBCS = true
 if(salary > 50000 && isBCS == true){
-    console.log('Sei lavel er patro')
+    // console.log('Sei lavel er patro')
 }else{
-    console.log('doore giay moro')
+    // console.log('doore giay moro')
+}
+const height = 68
+if(salary >80000 || height > 40){
+    // console.log('Bolo baba kobul')
+}else{
+    // console.log('vaag tui mokbul')
+}
+
+
+// if else page(80)
+// 1
+const age = 22
+if(age > 18 && height > 60){
+    // console.log('Push car')
+} else{
+    // console.log('sitting in the car')
+}
+
+// 2
+const math = 85
+const english = 81
+if(math > 80 || english > 85){
+    // console.log('aligible for admission')
+}else{
+    // console.log('I’ll get you married off!')
+}
+
+// 3
+const GPA = 5
+const income = 12000
+if(GPA == 5 && income <10000){
+    // console.log("You will get Scholarship")
+}else{
+    // console.log("You will not get Scholarship")
+}
+
+// 4
+const workAge = 28
+const workExperience = 1 
+if(workAge <30 && workExperience > 2){
+    // console.log("You are eligible for this job")
+} else{
+    // console.log("You are not eligible for this job")
+}
+
+// 5
+const egg = 10
+const chicken = true
+if(egg > 12 && chicken == true){
+    // console.log('I cook korma of egg')
+}else{
+    // console.log('I eat bun and banana')
+}
+
+// 6
+const tampera = 101
+const cough = true
+if(tampera > 100 || cough == true){
+    // console.log('I go to doctor')
+    }else{
+    // console.log('I take rest')
+    }
+
+// 7
+const present = 88
+const homework = true
+if(present > 80 && homework == true){
+    // console.log('You get permission to exam')
+}else{
+    // console.log('You are not allowed to exam')
+}
+
+// 8
+const electricity = false
+const mobileCharge = true
+if(electricity  && mobileCharge == true){
+    // console.log('I read a book')
+}else{
+    // console.log('I play a game')
+}
+
+// 9
+const shartPrice =1200
+const isCopun = true
+if(shartPrice > 1000 && isCopun == true){
+    // console.log('I get 20 percent discount')
+}else{
+    // console.log('I get 10 percent discount')
+}
+
+
+
+
+//**********************  Discount (else if)  ****************************  
+
+const price = 4000
+if(price > 5000){
+    const discount = price / 100 * 10
+    const pay = price - discount
+    // console.log(`You have to pay ${pay}`)
+}else{
+    // console.log(price)
+}
+
+if(price >=5000 ){
+    const discount = price / 100 * 10
+    const pay = price - discount
+    console.log(`You have to pay ${pay}`)
+} else if(price >= 2500 && price < 5000){
+    const discount = price / 100 * 5
+    const pay = price - discount
+    // console.log(`You have to pay ${pay}`) 
+} else{
+    // console.log(price)   
+}
+
+// page 83
+// 1
+const newPrice = 4500 
+if(newPrice >= 6000){
+    const discount = newPrice / 100 * 15
+    const pay = newPrice -discount
+    // console.log(`You have to pay ${pay}`)
+}else if(newPrice >= 3000){
+    const discount = newPrice /100 * 5
+    const pay = newPrice - discount
+    // console.log(`You have to pay ${pay}`)
+} else{
+    // console.log(newPrice)
 }
