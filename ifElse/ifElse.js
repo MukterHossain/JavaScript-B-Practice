f7426e8a832ca52c564f80280ab4bbf5a1cc33f6
MukterHossain <mukter3075@gmail.com>
2025-04-05 15:50:22 +0600

    ifElse operator

--- ifElse/ifElse.js
+++ ifElse/ifElse.js
@@ -2,16 +2,97 @@
 const x = 100
 const y = 100
 
-if(x > y){
-    console.log('x is greater than y')
-}
+// if(x > y){
+//     console.log('x is greater than y')
+// }
 
-else if(x < y){
-    console.log('Your is equal to jlsjdj')
-}
-else if(x == y && x > y){
-    console.log('x22 is less than 2')
+// else if(x < y){
+//     console.log('Your is equal to jlsjdj')
+// }
+// else if(x == y && x > y){
+//     console.log('x22 is less than 2')
+// }
+// else{
+//     console.log('y222111 is greater than 2')
+// }
+
+const rainingOutside = true
+if(rainingOutside === true){
+    // console.log('Take an umbrella')
 }
 else{
-    console.log('y222111 is greater than 2')
+    // console.log('No need for an umbrella')
+
+}
+
+const weight = 20
+if(weight >20 || weight === 20){
+    // console.log('I hire a rikshaw')
+}else{
+    // console.log('I walk to the destination')
+}
+
+
+// If Else page(77)
+// 1
+const commingTime = 5.30
+if(commingTime < 6){
+    // console.log('Mom give me launch')
+}else{
+    // console.log('Mom beat me with a stick')
+}
+
+// 2
+const login = true
+if(login === true){
+    // console.log('Welcome to website')
+}else{
+    // console.log('Get out')
+}
+
+// 3
+const distence = 5
+if(distence){
+    // console.log('I give you a chocolate')
+}else{
+    // console.log('Your are a obbes')
+}
+
+// 4
+const getMarks = 85
+if(getMarks > 80){
+    // console.log('I get a bike from my father') 
+}else{
+    // console.log('I have no bike')
+}
+
+// 5
+const movieShow = 8
+if(movieShow < 9){
+    // console.log('I watch a movie')
+}else{
+    // console.log('I go home and sleep')
+}
+
+// 6
+const tamperature = 30
+if(tamperature >= 30){
+    // console.log('Start the AC')
+}else{
+    // console.log('Start the fan')
+
+}
+
+const salary = 75000
+if(salary > 50000){
+    // console.log('Su Patro')
+}else{
+    // console.log('Nesxt 5 years single thako')
+}
+
+const isBCS = true
+if(salary > 50000 && isBCS == true){
+    console.log('Sei lavel er patro')
+}else{
+    console.log('doore giay moro')
 }
