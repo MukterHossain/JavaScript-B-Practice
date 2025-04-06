a9b09a04caa30018d488e79167971e3e67a83398
MukterHossain <mukter3075@gmail.com>
2025-04-06 21:43:27 +0600

    added loop

---
+++ loop/loop.js
@@ -0,0 +1,31 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for(const num of numbers){
+    const sum = num * 10
+    // console.log('Number : ', num, ',', sum)
+}
+
+
+// page (105)
+// 1
+const subjects = ['Bangla', 'Arabic', 'English', 'Urdu', 'Hindi', 'Parsi']
+for(const item of subjects){
+    // console.log('item: ', item)
+}
+
+// 2
+const foods = ['Vegitable', 'Poteto', 'Tometo', 'Chiken', 'Meat']
+for(const item of foods){
+    // console.log(item)
+}
+
+// 3
+const birthYear = [1970, 1985, 1993, 1996, 2014, 2017, 2024]
+for(const item of birthYear){
+    // console.log(item)
+}
+
+// 4
+const players = ['Sarif', 'Asraf', 'Mobarok', 'Rejaul Karim', 'Billal']
+for(const item of players){
+    // console.log(item)
+}
