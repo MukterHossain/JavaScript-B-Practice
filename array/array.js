7ba18b9b813b179e3e287086c82717b0de25d776
MukterHossain <mukter3075@gmail.com>
2025-04-06 17:34:47 +0600

    added array method

---
+++ array/array.js
@@ -0,0 +1,137 @@
+//  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  console.log(numbers.length)
+// page (93)
+// 1
+const newNumbers = [71, 72, 73, 74, 75, 76, 77, 78, 79]
+// console.log(newNumbers)
+
+// 2
+// const fruits = ['Apple', 'Banana', 'Mango', 'Papaya', 'Orange']
+// console.log(fruits)
+
+//3
+// const vegetables = ['আলু', 'Onion', 'Bagun', 'Mola', 'Gajor', 'Tometo', 'Darosh', 'Lau', 'Cucumber']
+// console.log(vegetables.length)
+
+
+
+// **************************  Index (95) ********************************
+// console.log(vegetables[5])
+// console.log(vegetables[15])
+// vegetables[3] = 'Bandi'
+// console.log(vegetables)
+
+
+// page (96)
+// 1
+// const friends = ['Sarif', 'Asraf', 'Mobarok', 'Rejaul Karim', 'Billal']
+// console.log(friends[3])
+// 2
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// numbers[6] = 30
+// console.log(numbers)
+
+// 3
+
+const gameName = ['Free fire', 'Pubji', 'Candi crush', 'Temple run']
+// console.log(gameName)
+// gameName[2] = 'Subway Run'
+// console.log(gameName)
+
+
+
+
+// ********************************** Push and Pop ******************************
+
+// const friends = ['Sarif', 'Asraf', 'Mobarok', 'Rejaul Karim', 'Billal']
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// numbers.push(12)
+// numbers.push(20, 22, 25, 3215, 251)
+// const gone =numbers.pop()
+// console.log(numbers)
+// console.log(gone)
+
+
+
+// ********************************** Shift and unsift ******************************
+// const friends = ['Sarif', 'Asraf', 'Mobarok', 'Rejaul Karim', 'Billal']
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// friends.shift()
+// friends.unshift('Morad')
+// console.log(friends)
+
+// page (99 and 100)
+// 1
+// const numbers = [10, 20, 30, 40, 50, 50]
+// numbers.push(60)
+// console.log(numbers)
+
+// 2
+// const friends = ['Sojib', 'Sagor', 'Sakib', 'Sohel']
+// friends.push('Somon', 'Ripon')
+// console.log(friends)
+
+// 3
+// friends.pop()
+// console.log(friends)
+
+// 4
+// const numbers = [24, 36, 48, 60]
+// numbers.unshift(12)
+// console.log(numbers)
+
+// 5
+// const books = ['Riple back', 'Boshonto', 'New Year', 'Moner Kotha']
+// books.shift()
+// console.log(books)
+
+
+
+
+
+
+// ********************************** Includes() and indexOf() ******************************
+
+// const friends = ['Sojib', 'Sagor', 'Sakib', 'Sohel', 'Kalam']
+// const exit = friends.includes('Sagor')
+// console.log(exit)
+// if(friends.includes('Kalam')){
+//     // console.log("He is good person")
+// } else{
+//     // console.log('He is not bad person')
+// }
+
+// console.log(friends.indexOf('Sakib')) // 2
+// console.log(friends.indexOf('Sohap')) // -1 (no data)
+
+
+
+// page (102)
+// 1
+const fruits = ['Apple', 'Banana', 'Mango', 'Papaya', 'Orange']
+// fruits.includes('Mango') ? console.log('Mango paisi') : console.log("Am nei")
+
+// 2
+const friends = ['Sojib', 'Babul', 'Alif', 'Rifat', 'Sagor', 'Sakib', 'Sohel', 'Kalam']
+// console.log(friends.indexOf('Babul'))
+
+// 3
+// console.log(friends.indexOf('Rifat'))
+
+// 4
+const cities = ['Dhaka', 'Chittagong', 'Sylhet']
+cities.push('rajshahi')
+// console.log(cities)
+
+// console.log(cities.indexOf('RajShahi'))
+
+// 5
+const weather = ['Dighi', 'বৃষ্টি', 'বর্ষা', 'শরৎ']
+// weather.includes('বৃষ্টি') ? console.log('I need umbrella') : console.log('No rain no pain')
+
+// 6 
+const sports = ['Football', 'Cricket', 'Voliball']
+console.log(sports.includes('Batminton'))
+
+
