cacd20df6670fdcb79718ca5f8348f3ea2a00473
MukterHossain <mukter3075@gmail.com>
2025-04-07 23:04:58 +0600

    added while loop for addition

--- loop/loop.js
+++ loop/loop.js
@@ -1,3 +1,5 @@
+
+ // *************************************** for --- of  loop *****************************************
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for(const num of numbers){
     const sum = num * 10
@@ -29,3 +31,62 @@ const players = ['Sarif', 'Asraf', 'Mobarok', 'Rejaul Karim', 'Billal']
 for(const item of players){
     // console.log(item)
 }
+
+
+
+// *************************************** While  loop *****************************************
+
+let num = 0
+while(num < 25){
+    // console.log(num)
+    num = num +1  //or num += 1  or num++
+}
+let schoolName = 0
+while(schoolName < 5){
+    // console.log('Bamboo Eating High School')
+    schoolName++
+}
+
+// page (109)
+// 1
+let workHour = []
+while(workHour < 10){
+    // console.log('I will practice programming minimum 3 hour a day')
+    workHour++
+}
+
+// 2 
+let num1 = []
+while(num1 < 20){
+    // console.log(num1)
+    num1++
+}
+
+// 3
+let num2 = 50
+while(num2  < 100 ){
+    // console.log(num2)
+    num2++
+} 
+
+// 4
+let num3 = 1
+while(num3 <11){
+    const sum = num3 * 3
+    // console.log(sum)
+    num3++
+
+}
+
+// 5
+let num4 = 110
+while(num4 <111 ){
+    const sum = num4 /2
+    // console.log(sum)
+    num4++
+}
+
+
+
+
+// *************************************** While (Add/যোগ) *****************************************
