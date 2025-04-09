a701e76886945658aacb5243bd1446e1ec0551e9
MukterHossain <mukter3075@gmail.com>
2025-04-09 22:11:12 +0600

    added object

---
+++ object/object.js
@@ -0,0 +1,45 @@
+ const students = {
+    name: 'Mukter Hossain',
+    age: 15,
+    class: 12,
+    isSingle: false
+ }
+//  console.log(students)
+
+// page(126)
+// 1
+const teacher = {
+   name: 'Abusaed',
+   teaches: 'English',
+   age: 45,
+   live: 'Sonargaon',
+   isMarried: true
+
+}
+
+const trees ={
+   name: 'Maongo tree',
+   height: '56 feet',
+   age:25,
+   fruit: 'Mango'
+}
+// console.log(teacher, trees)
+
+
+
+// *************************************** object  (key of property) *******************************
+
+const person = {
+   name: 'soldor uddain',
+   age: 25, 
+   profession: 'developer',
+   sallry: 25000,
+   married: true,
+   'fav place': ['bandarban', 'saintmartin', 'kuakata'],
+   education: {
+      title: ['SSC', 'HSC', 'BSC', 'MSC'],
+      year: [2011, 2013, 2017, 2018]
+   }
+}
+console.log(person)
+
