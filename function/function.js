da2f4d03db0d03cd247cb0590756737bfd6e9942
MukterHossain <mukter3075@gmail.com>
2025-04-21 17:47:34 +0600

    added function method

---
+++ function/function.js
@@ -0,0 +1,11 @@
+// function mama(){
+//     console.log('Hello')
+// }
+// mama()
+const mama = () =>{
+    console.log('Hello')
+    console.log('Hello 2 ')
+    console.log('Hello 3')
+    console.log('Hello 4')
+}
+mama()
