b5edd95b2a97fcb1b89b900cf9640ab285833dd9
MukterHossain <mukter3075@gmail.com>
2025-04-24 12:31:14 +0600

    added some function code

--- function/function.js
+++ function/function.js
@@ -11,11 +11,74 @@
 // mama()
 
 
-function myData(num, sum){
-    const result= num * 2
-    const tatal = result + sum
-    console.log(result , tatal)
-}
+// function myData(num, sum){
+//     const result= num * 2
+//     const tatal = result + sum
+//     // console.log(result , tatal)
+// }
 
-myData(2, 10)
+// myData(2, 10)
+
+// function squre(num){
+//     const number = num * num
+//     // console.log(number)
+// }
+// squre(20)
+
+// function add(a, b, c, d){
+//     const total = a + b + c + d
+//     console.log(a, b , c ,d)
+//     console.log(total)
+// }
+// add(1, 2, 'm', 'u')
+
+// function difference(num1, num2){
+//     const diff = num1 - num2
+//     console.log(diff)
+// }
+// const fatherAge=65
+// const myAge = 30
+// difference(fatherAge, myAge)
+
+
+// function neAdd(a, b){
+//     console.log(arguments)
+//     let sum =0
+//     for(const num of arguments){
+//         sum = sum + num
+//     }
+//     console.log(sum)
+//     return a +b
+    
+// }
+
+// const result = neAdd(1, 2, 3, 4)
+// console.log(result)
+
+
+// page(149)
+// 1
+// function age(num1, num2){
+//     return num1 + num2;
+// }
+// const fatherAge = 50;
+// const sonAge = 20
+// const result = age(fatherAge, sonAge)
+// console.log(result)
+
+
+// 2
+// function multiple(num1, num2){
+//     return num1 * num2
+// }
+// const result =multiple(20, 15)
+// console.log(result)
+
+// 3
+// function examResult(num1, num2, num3){
+//     return num1 + num2 + num3
+// }
+// const result = examResult(80, 85, 75)
+// console.log(result)
+
 
