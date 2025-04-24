84c7a4efc4f0121fce4beec81b3642720c665d8e
MukterHossain <mukter3075@gmail.com>
2025-04-24 22:43:46 +0600

    added som return code in function

--- function/function.js
+++ function/function.js
@@ -82,3 +82,59 @@
 // console.log(result)
 
 
+
+//4
+// function year(nowYear, myBirth){
+//     console.log(nowYear - myBirth)
+// }
+// year(2025, 1996)
+
+// 5
+// function mean(a, b, c, d){
+//     const sum = a + b + c + d
+//     return sum / 4
+// }
+// const result = mean(2, 6, 4, 8)
+// console.log(result)
+
+// 7 
+
+
+
+// ****************************************** Return **********************************************
+
+
+// const potato = 35
+// const onion = 62
+// const shirt = 500
+// const pant = 1250
+// function add(price1, price2){
+//     return price1 + price2
+// }
+// const bill1= add(potato, onion)
+// const bill2 = add(shirt, pant)
+// const finall = add(bill1, bill2)
+// console.log(finall)
+
+// function doMath(num1, num2){
+//     const sum = num1 + num2
+//     const diff = num1 - num2
+//     const multiple = sum * diff 
+//     const result = multiple / 5
+//     return result;
+// }
+
+// const result = doMath(50, 10)
+// console.log(result)
+
+// function isEven(num){
+//     if(num % 2 === 0){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(isEven(11)) // false
+// console.log(isEven(10)) // true
+
+// Page(153)
