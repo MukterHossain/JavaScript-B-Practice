91e899665aa35a673ce14f3a34899ba35042d7d8
MukterHossain <mukter3075@gmail.com>
2025-04-28 22:40:18 +0600

    added es6 some logic

---
+++ es6/es6.js
@@ -0,0 +1,61 @@
+ 
+//  let money = 10
+//   money = money +12
+//  console.log(money)
+
+// const numbers = [1, 2, 3, 4, 5, 6]
+// numbers[2] = 20
+// numbers.push(25, 23, 28)
+// console.log(numbers)
+
+// const student = {
+//     name: 'Mukter',
+//     age: 29,
+
+// }
+// student.name = 'Mukter Hossain'
+// console.log(student)
+
+// ******************************************* Default *************************************
+// function add(num1 = 0, num2 = 0){
+//     const result = num1 + num2
+//     console.log(num1, num2, result)
+//     return result
+// }
+// console.log(add(5))
+// console.log(add(5, 6))
+
+// page (186)
+// 1
+// function add(num1 = 0, num2 = 0, num3 = 0){
+//     const result = num1 + num2 + num3
+//     return result
+// }
+// console.log(add(1, 5, 6))
+
+// 2
+// function save(money = 50){
+//     return money
+// }
+// console.log(save())
+// console.log(save(150))
+
+// 3
+// function member(name = 'anonymous', sellary = 0){
+//     const result = name + ',' + sellary
+//     return result
+// }
+// console.log(member('Mukter', 1200))
+// console.log(member())
+// console.log(member('Mukter'))
+// console.log(member(1, 1200))
+
+
+
+
+
+
+
+
+// ******************************************* Default *************************************
+// ******************************************* Default *************************************
