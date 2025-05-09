dabc373b06d6057f3e42d6c281e648b2926dd258
MukterHossain <mukter3075@gmail.com>
2025-05-09 21:53:40 +0600

    added destructure method

---
+++ destructure/destructure.js
@@ -0,0 +1,20 @@
+ 
+ const person = {
+    name: "Hasib",
+    age:20,
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh",
+        zip: "1212"
+    },
+    education: {
+        school: "XYZ High School",
+        college: "ABC College",
+        university: "DEF University"
+    },
+    hobbies: ["reading", "traveling", "coding"]
+ }
+
+
+ const {name, age } = person;
+ console.log(name, age)
