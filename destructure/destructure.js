aec15030fbe2a7b8867f68cef83944a0cd143043
MukterHossain <mukter3075@gmail.com>
2025-08-05 22:53:44 +0600

    added some destructure method

--- destructure/destructure.js
+++ destructure/destructure.js
@@ -1,48 +1,117 @@
  
- const person = {
-    name: "Hasib",
-    age:20,
-    address: {
-        city: "Dhaka",
-        country: "Bangladesh",
-        zip: "1212"
-    },
-    education: {
-        school: "XYZ High School",
-        college: "ABC College",
-        university: "DEF University"
-    },
-    hobbies: ["reading", "traveling", "coding"]
- }
+//  const person = {
+//     name: "Hasib",
+//     age:20,
+//     address: {
+//         city: "Dhaka",
+//         country: "Bangladesh",
+//         zip: "1212"
+//     },
+//     education: {
+//         school: "XYZ High School",
+//         college: "ABC College",
+//         university: "DEF University"
+//     },
+//     hobbies: ["reading", "traveling", "coding"]
+//  }
 
 
- const {name, age, education, hobbies } = person;
-//  console.log(name, age)
- const {school:newSchool, college, university} = education
-//  console.log(school, college, university)
+//  const {name, age, education, hobbies } = person;
+// //  console.log(name, age)
+//  const {school:newSchool, college, university} = education
+// //  console.log(school, college, university)
 
- const numberF =('mama')
-//  console.log(numberF,  education.school)
-const [a, b, c] = hobbies;
-const add = person.education.college;
-// console.log(add)
-// console.log(newSchool)
-// console.log(a, b, c)
+//  const numberF =('mama')
+// //  console.log(numberF,  education.school)
+// const [a, b, c] = hobbies;
+// const add = person.education.college;
+// // console.log(add)
+// // console.log(newSchool)
+// // console.log(a, b, c)
 
-function doubleTehm(a, b){
-    return [a *2, b * 2]
-}
-const [pro, dit] = doubleTehm(5, 10);
-// console.log(pro, dit)
+// function doubleTehm(a, b){
+//     return [a *2, b * 2]
+// }
+// const [pro, dit] = doubleTehm(5, 10);
+// // console.log(pro, dit)
 
-const person1 = {name1: "Amit", age:25};
-const {name1, phone = "N/A"} = person1;
-// console.log(name1, phone)
+// const person1 = {name1: "Amit", age:25};
+// const {name1, phone = "N/A"} = person1;
+// // console.log(name1, phone)
 
-function introduce({name, age}){
-    console.log(`Name: ${name}, Age: ${age}`)
-}
+// function introduce({name, age}){
+//     // console.log(`Name: ${name}, Age: ${age}`)
+// }
 
-const user = {name: "Arohi", age:20, hobby: "Reading"}
-introduce(user);
+// const user = {name: "Arohi", age:20, hobby: "Reading"}
+// introduce(user);
 
+// function sum([a, b]){
+//     return a + b
+// }
+
+// const data = [3, 6]
+// const result= sum(data);
+// // console.log(result)
+
+// const numbers = [1, 2, 3, 4, 5]
+// const [, ma, , titi, mon] = numbers
+
+// // console.log(ma, titi, mon)
+
+
+// // Pratice 202 page
+
+// // 1
+// const product = {
+//     name: "Laptop",
+//     price: 50000,
+//     brand: "Dell"
+// }
+
+// // console.log(product.brand)
+
+// // 2
+// const item = {
+//     name: "mobile",
+//     price: 20000,
+//     phone: "Sansung"
+// }
+
+// const {price, phone: myPhone} = item
+
+// // console.log(`Price: ${price}, Phone: ${myPhone}`)
+// //3
+// const colors = ["red", "blue", "green", "yellow"]
+
+// const [first, second] = colors
+// // console.log(first, second)
+
+// //4
+
+// const thisNumber = [2, 5, 324, 325, 25]
+// const secondNumber = thisNumber[1]
+// // console.log(secondNumber)
+
+// // 5
+// const digits = [2, 4, 6, 8]
+
+// const tow = digits[0]
+// const eight =digits[3]
+// // console.log(tow, eight)
+
+// // 6
+// function multiply(a,b){
+// return [a *3, b*3]
+// }
+// const [three, six] = multiply(1, 2);
+// // console.log(three, six)  
+
+// // 7
+// const person2 = {name: "Rahim", city: "Dhaka"}
+// const {name: personName, city: personCity, thisPhone = "N/A"} = person2;
+// console.log(`Name: ${personName}, City: ${personCity}, Phone: ${thisPhone}`)
+
+
+
+//  *********************** Rest operator *********************
