38583d8bb218730203da29232e4398b926e18e41
MukterHossain <mukter3075@gmail.com>
2025-09-05 21:45:24 +0600

    added filter, map, find,forEach

--- arrayMethod/array.js
+++ arrayMethod/array.js
@@ -51,12 +51,80 @@
 
 
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const friends = ["Tom", "Jhon", "Sakib", "Ripon", "Sohel"]
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const friends = ["Tom", "Jhon", "Sakib", "Ripon", "Sohel"]
 
 
 // const result = numbers.find(num => num > 15)
 // console.log(result)
-const result = numbers.findIndex(num => num > 8)
-console.log(result)
+// const result = numbers.findIndex(num => num > 8)
+// console.log(result)
+
+// const students = [
+//     {name: "Jhon", age: 29},
+//     {name: "Jhon", age: 29},
+//     {name: "Jhon", age: 29},
+//     {name: "Jhon", age: 29},
+//     {name: "Jhon", age: 29},
+// ]
+
+// const result = students.map((students, index, array) => {
+//     return `${students.name}, ${index + 1}, of ${array.length} students`
+// })
+
+// console.log(result)
+
+
+// practice 221 page
+// 1 
+// const snatch = [30, 45, 20, 60, 10]
+// const result = snatch.map(cost => cost + 13)
+// console.log(result)
+
+// 2
+// const friends = ["Tom", "Jhon", "Sakibul", "Ripon", "Sohel", "Maradona"]
+
+// const result = friends.filter(friend => friend.length > 5)
+// console.log(result)
+
+// 3
+// const numbers = [9, 15, 25, 32, 10, 7]
+// const result = numbers.find(num => num > 20)
+// console.log(result)
+
+// 4
+// const heights = [65, 70, 68, 72, 68, 73]
+// const result = heights.filter(num => num > 69)
+// console.log(result)
+
+// 5
+// const numbers = [7, 10, 15, 20, 25, 30]
+// const result = numbers.map(num => (num /3).toFixed(0))
+// console.log(result)
+
+// 6
+// const friends = ["Leonardo", "Brad Pitt", "Tom Cruise", "Tom Hanks", "Johnny Depp"]
+// const result = friends.map(friend => friend[2])
+// console.log(result)
+
+// 7
+// const names = ["Tom", "Harry", "Sam", "Jack"]
+// const result = names.find(name => name[0] === "H")
+// console.log(result)
+
+// 8
+// const numbers = [25, 30, 45, 60, 75, 80]
+// const result = numbers.forEach(num=> console.log(num))
+
+// console.log(result)
+
+// 9
+// const animals = ["cat", "dog", "elephant", "bee", "ant"]
+// const result = animals.forEach(animal => console.log(animal))
+// console.log(result)
+
+
+//************************** some and every  ****************** */
+
+
 
