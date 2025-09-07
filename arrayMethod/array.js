97b7cd0dcc75c8216f5f31bc1e339db09e3f97b5
MukterHossain <mukter3075@gmail.com>
2025-09-07 22:22:46 +0600

    added reduce

--- arrayMethod/array.js
+++ arrayMethod/array.js
@@ -162,3 +162,9 @@
 
 //************************** reduce  ****************** */
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let sum = 0
+for(const num of numbers){
+    sum = sum + num
+}
+console.log(sum)
